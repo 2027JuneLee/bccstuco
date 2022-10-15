4daa085bc4fb9f4d932864dc5a3e9390509f12bc
ellie <ellie@kmong.com>
2022-10-15 13:11:14 +0900

    reports page

--- src/WeeklyReports/index.js
+++ src/WeeklyReports/index.js
@@ -45,27 +45,30 @@ const Link = styled.a`
 const BlockWrapper = styled.div`
   display: flex;
   justify-content: center;
+  align-items: center;
   flex-direction: column;
-  
+  margin-top: 50px;
 `;
 
-const BlockRow = styled.div`
-display:flex;
-flex-direction:column;
-`;
-
-const Block = styled.div`
-  width: 40%;
-  height: 100%auto;
-  padding: 20px;
-  background-color: #023e8a;
-  border-radius: 5px;
-  color: white;
-`;
-const BlockTItle = styled.div`
-
-  color: #1d3557;
+const BlockTitle = styled.div`
+  color: black;
   font-size: 50px;
+  border-bottom: 2px solid #1d3557;
+  width: 50%;
+  text-align: center;
+  margin-top: 20px;
+  margin-bottom: 20px;
+`;
+
+const Month = styled.div`
+  color: #023e8a;
+  font-size: 45px;
+  margin-top: 10px;
+  font-weight: 700;
+  font-family: "Fuzzy Bubbles", cursive;
+`;
+const MonthLink = styled.a`
+  text-decoration: none;
 `;
 
 function WeeklyReports() {
@@ -150,10 +153,18 @@ function WeeklyReports() {
           Community
         </Link>
         <a href="mailto:stuco@usbccollegiate.org" target="_blank">
-        <img width="50px" height="30px" src="https://th.bing.com/th/id/R.88d88f1872909358b97ffa695cb97b66?rik=A77az%2fwRYB%2bLrw&riu=http%3a%2f%2flogos-download.com%2fwp-content%2fuploads%2f2016%2f05%2fGmail_logo_icon.png&ehk=8gYQfqc8h6fYCXddKEQE%2blQ7004YVwPu1S2LSHmiUgE%3d&risl=&pid=ImgRaw&r=0"></img>
+          <img
+            width="50px"
+            height="30px"
+            src="https://th.bing.com/th/id/R.88d88f1872909358b97ffa695cb97b66?rik=A77az%2fwRYB%2bLrw&riu=http%3a%2f%2flogos-download.com%2fwp-content%2fuploads%2f2016%2f05%2fGmail_logo_icon.png&ehk=8gYQfqc8h6fYCXddKEQE%2blQ7004YVwPu1S2LSHmiUgE%3d&risl=&pid=ImgRaw&r=0"
+          ></img>
         </a>
         <a href="https://www.instagram.com/bcc_stuco/" target="_blank">
-          <img width="45px" height="45px" src="https://th.bing.com/th/id/OIP.JtmXSh_uyZBaTg1eXd-NtgHaHa?pid=ImgDet&rs=1"></img>
+          <img
+            width="45px"
+            height="45px"
+            src="https://th.bing.com/th/id/OIP.JtmXSh_uyZBaTg1eXd-NtgHaHa?pid=ImgDet&rs=1"
+          ></img>
         </a>
       </Menu>
 
@@ -166,45 +177,24 @@ function WeeklyReports() {
         />
         <Title>Weekly Reports!</Title>
       </HeaderWrapper>
-      <Blank
-          src={
-            "https://fortbendseniors.org/wp-content/uploads/2019/01/blank-white-square-thumbnail.jpg"
-          }
-        />
+
       <BlockWrapper>
-      
-        <BlockTItle>2022</BlockTItle>
-        <BlockRow>
-        <Blank
-          src={
-            "https://fortbendseniors.org/wp-content/uploads/2019/01/blank-white-square-thumbnail.jpg"
-          }
-        />
-        </BlockRow>
-        
-        <BlockRow>
-          <a href="https://drive.google.com/drive/folders/1YUvUPJGVwspFY7tI1SY7HttHU52ivkOe" target="_blank">
-          <Block>SEPTEMBER</Block>
-          </a>
-          <br></br>
-          <a href="https://drive.google.com/drive/folders/1kdujEiqiME5cDrJLZ-wbBgOn1dsNqBp-" target="_blank">
-          <Block>OCTOBER</Block>
-          </a>
-          <br></br>
-          <a href="https://drive.google.com/drive/folders/1CRbBgYsIxF0ALuyb54xWkfa4IUWjvkPe" target="_blank">
-          <Block>STUCO DRIVE</Block>
-          <br></br>
-          </a>
-          <Block>...</Block>
-        </BlockRow>
-        <BlockRow>
-        <Blank
-          src={
-            "https://fortbendseniors.org/wp-content/uploads/2019/01/blank-white-square-thumbnail.jpg"
-          }
-        />
-        </BlockRow>
-    
+        <BlockTitle>2022</BlockTitle>
+        <MonthLink
+          href="https://drive.google.com/drive/folders/1YUvUPJGVwspFY7tI1SY7HttHU52ivkOe"
+          target="_blank"
+        >
+          <Month>SEPTEMBER</Month>
+        </MonthLink>
+        <MonthLink
+          href="https://drive.google.com/drive/folders/1kdujEiqiME5cDrJLZ-wbBgOn1dsNqBp-"
+          target="_blank"
+        >
+          <Month>NOVEMBER</Month>
+        </MonthLink>
+        <Month>DECEMBER</Month>
+        <BlockTitle>2023</BlockTitle>
+        <Month>To be added...</Month>
       </BlockWrapper>
     </Wrapper>
   );
